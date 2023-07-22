ec661dfed00b9b1f003afdef63b28e653391a057
srisais <electronic-mail>
2023-07-21 23:19:18 -0400

    Added some details to the java files about what they are

--- src/main/java/2_application_business_rules/boundaries/MainBoundary.java
+++
@@ -1,3 +0,0 @@
-this main boundary is the only boundary that the outside layers are gonna be talking to
-
-then, this boundary will talk to the other two boundaries!
--- src/main/java/2_application_business_rules/boundaries/ProjectSelectionBoundary.java
+++
@@ -1,3 +0,0 @@
-so for this boundary, the MainBoundary will talk to this boundary.
-
-then, this boundary will be responsible for calling the use cases (for the project selection stuff)
