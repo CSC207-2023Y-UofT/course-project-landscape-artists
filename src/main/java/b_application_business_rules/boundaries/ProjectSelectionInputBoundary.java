016c2776b179c4bb09c9941f3389f58206fd1562
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-01 18:31:54 -0400

    I/O boundaries changed for create/delete project use cases.

--- src/main/java/b_application_business_rules/boundaries/ProjectSelectionInputBoundary.java
+++ src/main/java/b_application_business_rules/boundaries/ProjectSelectionInputBoundary.java
@@ -1,8 +1,19 @@
 package b_application_business_rules.boundaries;
 
 import a_enterprise_business_rules.entities.Project;
+import b_application_business_rules.entity_models.ProjectModel;
+
+import java.util.UUID;
 
 public interface ProjectSelectionInputBoundary {
     void setCurrentProject(Project project);
     void createProject(String name, String description);
+
+    void createProject(ProjectModel projectModel);
+
+    void deleteProject(UUID projectID);
+
+    void projectDeletionFailed(String message);
+
+    void projectDeleted(UUID projectID);
 }
--- src/main/java/b_application_business_rules/boundaries/ProjectSelectionOutputBoundary.java
+++ src/main/java/b_application_business_rules/boundaries/ProjectSelectionOutputBoundary.java
@@ -1,5 +1,8 @@
 package b_application_business_rules.boundaries;
+import b_application_business_rules.entity_models.ProjectModel;
 
 public interface ProjectSelectionOutputBoundary {
     void displayCurrentProject();
+    void projectCreated(ProjectModel projectModel);
+    void projectCreationFailed(String errorMessage);
 }
