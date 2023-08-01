c1daf39c79f148f35f32d9c881bea9f53fb0917f
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-01 18:17:13 -0400

    CreateProject use case. Requires resolving conflict between list<column> and list<columnmodel>

--- src/main/java/b_application_business_rules/use_cases/project_selection_use_cases/CreateProject.java
+++ src/main/java/b_application_business_rules/use_cases/project_selection_use_cases/CreateProject.java
@@ -1,4 +1,48 @@
 package b_application_business_rules.use_cases.project_selection_use_cases;
+import a_enterprise_business_rules.entities.Project;
+import b_application_business_rules.boundaries.ProjectSelectionInputBoundary;
+import b_application_business_rules.boundaries.ProjectSelectionOutputBoundary;
+import b_application_business_rules.entity_models.ProjectModel;
+import b_application_business_rules.entity_models.ColumnModel;
+import java.util.List;
 
-public class CreateProject {
-};
+public abstract class CreateProject implements ProjectSelectionInputBoundary {
+    private ProjectSelectionOutputBoundary outputBoundary;
+
+    public CreateProject(ProjectSelectionOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
+    }
+
+    @Override
+    public void createProject(ProjectModel projectModel){
+        try{
+            // Create the new Project entity
+            Project project = createProjectEntity(projectModel);
+            Project project = new Project(projectModel.getName(), projectModel.getID(), projectModel.getDescription(),
+                    projectModel.getColumnModels());
+
+            // Notify via the output boundary if created successfully
+            outputBoundary.projectCreated(new ProjectModel(project.getName(), project.getID(), project.getDescription(),
+                    project.getColumns())); // ???
+        } catch (Exception e){
+            // Notify via the output boundary if cretion failed
+            outputBoundary.projectCreationFailed(e.getMessage());
+        }
+    }
+
+    // Validate projectModel data
+    private void validateProjectModel(ProjectModel projectModel) {
+        if (projectModel.getName() == null || projectModel.getName().isEmpty()){
+            throw new IllegalArgumentException("Project name cannot be empty.");
+        }
+
+        // to be continued
+    }
+
+    // Create the Project entity from the ProjectModel (Project factory ?)
+    private Project createProjectEntity(ProjectModel projectModel) {
+        return new Project(projectModel.getName(), projectModel.getID(), projectModel.getDescription(),
+                projectModel.getColumnModels()); // ???
+    }
+
+}
