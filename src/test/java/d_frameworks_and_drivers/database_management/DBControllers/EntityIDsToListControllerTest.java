022a0b84352af7da8398bb60ed9993a09f220945
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-04 17:42:39 -0400

    Generating unit tests for database controllers using JUnit5 with setUp and tearDown.

---
+++ src/test/java/d_frameworks_and_drivers/database_management/DBControllers/EntityIDsToListControllerTest.java
@@ -0,0 +1,26 @@
+package d_frameworks_and_drivers.database_management.DBControllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntityIDsToListControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void entityIDsToList() {
+    }
+
+    @Test
+    void testEntityIDsToList() {
+    }
+}
---
+++ src/test/java/d_frameworks_and_drivers/database_management/DBControllers/EntityIDsToModelControllerTest.java
@@ -0,0 +1,22 @@
+package d_frameworks_and_drivers.database_management.DBControllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntityIDsToModelControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void IDstoProjectModelList() {
+    }
+}
