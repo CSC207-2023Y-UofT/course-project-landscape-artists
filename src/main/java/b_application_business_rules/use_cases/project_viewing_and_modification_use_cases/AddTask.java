fd199a5613493d23aed5e6e49c1b6a687069cb40
Wendy Wan <wendy.wan@mail.utoronto.ca>
2023-08-04 07:34:22 -0400

    implemented adding the task entities to the respective column entities!

--- src/main/java/b_application_business_rules/use_cases/project_viewing_and_modification_use_cases/AddTask.java
+++ src/main/java/b_application_business_rules/use_cases/project_viewing_and_modification_use_cases/AddTask.java
@@ -1,47 +1,48 @@
 package b_application_business_rules.use_cases.project_viewing_and_modification_use_cases;
 
-import a_enterprise_business_rules.entities.Project;
-import a_enterprise_business_rules.entities.Task;
-import b_application_business_rules.DataAccessInterface;
+import a_enterprise_business_rules.entities.*;
 
-import b_application_business_rules.entity_models.ProjectModel;
 import b_application_business_rules.entity_models.TaskModel;
+import b_application_business_rules.use_cases.CurrentProjectRepository;
 import b_application_business_rules.use_cases.project_selection_gateways.IDBInsert;
 import d_frameworks_and_drivers.database_management.DBControllers.DBManagerInsertController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
- * A use case class for editing task details (name and description)
+ * A use case class for creating new tasks in a column
  */
-public class AddTask implements DataAccessInterface {
-    TaskModel taskModel;
+public class AddTask {
+    private TaskModel taskModel;
+    private UUID idOfColumn;
+    //I'm not sure if this 'currentProject' will refer to the same project as other usecases would
+    private final Project currentProject = CurrentProjectRepository.getInstance().getCurrentProject().getProjectEntity();
 
-    public AddTask(TaskModel model) {
+    public AddTask(UUID idOfColumn, TaskModel model) {
         this.taskModel = model;
+        this.idOfColumn = idOfColumn;
     }
 
     /**
      * This method creates the task and calls the method that will add the task to the database
-     * NEED TO IMPLEMENT ADDING TASK TO THE LIST OF TASKS IN THE COLUMN ENTITY
      */
     public void addTask() {
         // Create task entity
         Task task = createTaskEntity(taskModel);
-        //Call the method that accesses the database
-        addNewTask(taskModel);
-    }
 
-    /**
-     * Adds the newly created task to the database
-     * @param newTask
-     */
-    @Override
-    public void addNewTask(TaskModel newTask) {
-        //Initializing the required controllers
+        // Add task to Column Entity
+        // First get the list of columns in the current project
+        ArrayList<Column> listOfColumns = currentProject.getColumns();
+        // Then search for the column entity
+        Column currentColumn = Column.IDToColumn(idOfColumn, listOfColumns);
+        // Then add the task to the columns list of tasks
+        currentColumn.getTasks().add(task);
+
+        // Initializing the required controllers and calls method that adds task to the database
         IDBInsert insertTask = new DBManagerInsertController();
-        insertTask.DBInsert(newTask);
+        insertTask.DBInsert(taskModel);
     }
 
     /**
@@ -49,8 +50,10 @@ public class AddTask implements DataAccessInterface {
      * @param taskModel
      */
 
-    private Task createTaskEntity(TaskModel taskModel) {
+    public static Task createTaskEntity(TaskModel taskModel) {
         return new Task(taskModel.getName(), taskModel.getID(), taskModel.getDescription(),
                 taskModel.getCompletionStatus(), taskModel.getDueDateTime());
     }
+
+
 }
