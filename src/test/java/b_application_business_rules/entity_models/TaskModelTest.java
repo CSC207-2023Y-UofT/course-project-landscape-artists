fb0fcf6ea657207e9979830c42765ac6032561f5
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-04 17:48:22 -0400

    Generating unit tests for entity models using JUnit5 with setUp and tearDown.

---
+++ src/test/java/b_application_business_rules/entity_models/TaskModelTest.java
@@ -0,0 +1,60 @@
+package b_application_business_rules.entity_models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskModelTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void setID() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getCompletionStatus() {
+    }
+
+    @Test
+    void completeTaskModel() {
+    }
+
+    @Test
+    void incompleteTaskModel() {
+    }
+
+    @Test
+    void negateCompletionStatus() {
+    }
+
+    @Test
+    void getDueDateTime() {
+    }
+
+    @Test
+    void setDueDateTime() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
