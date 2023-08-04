0f9eb14c1f9c21e2b3dccc8e3f9e87334b20a890
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-04 17:43:50 -0400

    Generating unit tests for UUIDMap using JUnit5 with setUp and tearDown.

---
+++ src/test/java/d_frameworks_and_drivers/database_management/UUIDsToHashMap/UUIDMapTest.java
@@ -0,0 +1,22 @@
+package d_frameworks_and_drivers.database_management.UUIDsToHashMap;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UUIDMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void convertCsvToHashMap() {
+    }
+}
