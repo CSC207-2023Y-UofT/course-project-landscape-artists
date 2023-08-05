0356693c3dc475ffd48c7e7057ace09580798882
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-05 17:55:39 -0400

    CSVWriter frameworks and drivers class that provides methods with application specific settings and uses

---
+++ src/main/java/d_frameworks_and_drivers/database_management/DBAdapters/CSVWriter.java
@@ -0,0 +1,132 @@
+package d_frameworks_and_drivers.database_management.DBAdapters;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVWriter implements AutoCloseable {
+    protected File csvFile;
+    private FileWriter fileWriter;
+    private CSVPrinter csvPrinter;
+
+    /**
+     * Creates a new CSVWriter given the filePath to csv to read.
+     * @param filePath
+     */
+    public CSVWriter(String filePath) throws FileNotFoundException {
+        this.csvFile = new File(filePath);
+    }
+
+    /**
+    * Opens the resources and initializes the fields.
+    *
+    * @throws IOException If an I/O error occurs.
+     */
+    private void openResources() throws IOException {
+        fileWriter = new FileWriter(csvFile);
+        csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader().withNullString(""));
+    }
+
+    /**
+     * Adds a list of String as a record into csv file.
+     *
+     * @param inputRecord a list of strings
+     */
+    public void insert(List<String> inputRecord){
+        // Get the header size from the CSV file, assuming the CSV file has a header
+        int headerNumber = -1;
+
+        try (CSVParser csvParser = new CSVParser(new FileReader(csvFile), CSVFormat.DEFAULT.withHeader().withNullString(""))) {
+            headerNumber = csvParser.getHeaderMap().size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Check if the input and csv file dont match
+        if (headerNumber != inputRecord.size()) {
+            throw new IllegalArgumentException("Number of inputted fields doesn't match number of csv headers.");
+        }
+
+        // Try with resource: create FileWriter and csvPrinter object as resources - closes automatically
+        try (FileWriter outputfile = new FileWriter(csvFile);
+             CSVPrinter csvPrinter = new CSVPrinter(outputfile, CSVFormat.DEFAULT.withHeader().withNullString(""))) {
+
+            // writes a new row/record to the CSV file
+            csvPrinter.printRecord(inputRecord);
+            csvPrinter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Adds a list of String as a record into csv file.
+     *
+     * @param strings one or more string parameters
+     */
+    public void insert(String... strings){
+        if (strings.length == 0){
+            throw new IllegalArgumentException("At least one argument is required.");
+        }
+
+        List<String> inputRecord = new ArrayList<>();
+
+        for (String s : strings){
+            inputRecord.add(s);
+        }
+
+        // Get the header size from the CSV file, assuming the CSV file has a header
+        int headerNumber = -1;
+
+        try (CSVParser csvParser = new CSVParser(new FileReader(csvFile), CSVFormat.DEFAULT.withHeader().withNullString(""))) {
+            headerNumber = csvParser.getHeaderMap().size();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Check if the input and csv file dont match
+        if (headerNumber != inputRecord.size()) {
+            throw new IllegalArgumentException("Number of inputted fields doesn't match number of csv headers.");
+        }
+
+        // Try with resource: create FileWriter and csvPrinter object as resources - closes automatically
+        try (FileWriter outputfile = new FileWriter(csvFile);
+             CSVPrinter csvPrinter = new CSVPrinter(outputfile, CSVFormat.DEFAULT.withHeader().withNullString(""))) {
+
+            // writes a new row/record to the CSV file
+            csvPrinter.printRecord(inputRecord);
+            csvPrinter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Closes the resources and performs cleanup.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
+    @Override
+    public void close() throws IOException {
+        // Close the CSVPrinter and FileWriter
+        if (csvPrinter != null) {
+            csvPrinter.close();
+        }
+        if (fileWriter != null) {
+            fileWriter.close();
+        }
+        // Set to null to indicate t closed
+        csvPrinter = null;
+        fileWriter = null;
+    }
+}
+
+
+
+
+
