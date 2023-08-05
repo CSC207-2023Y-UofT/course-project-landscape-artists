08326ce9dfc502d852112dbccb9e1951bd17a9dc
srisais <electronic-mail>
2023-08-04 21:00:28 -0400

    Added documentation and refactored CurrentProjectRepository

--- src/main/java/b_application_business_rules/use_cases/CurrentProjectRepository.java
+++ src/main/java/b_application_business_rules/use_cases/CurrentProjectRepository.java
@@ -1,37 +1,41 @@
 package b_application_business_rules.use_cases;
 
-import a_enterprise_business_rules.entities.Project;
 import b_application_business_rules.entity_models.ProjectModel;
-
-import java.util.UUID;
-
+s
+/**
+ * This singleton class will be the respository to store the current project
+ * that use cases will access utilize.
+ */
 public class CurrentProjectRepository {
-    private static final CurrentProjectRepository instance =
-            new CurrentProjectRepository();
+    /** The single instance of this singleton class */
+    private static final CurrentProjectRepository currentProjectRepository = new CurrentProjectRepository(null);
 
+    /** The current project that this repository is holding */
     private ProjectModel currentProject;
 
-    public static CurrentProjectRepository getInstance() {
-        return instance;
+    /** Constructor for the singleton class */
+    public CurrentProjectRepository(ProjectModel currentProject) {
+        this.currentProject = currentProject;
     }
 
-    private CurrentProjectRepository(){}
+    /** Gets the singleton instance of CurrentProjectRepository */
+    public static CurrentProjectRepository getCurrentprojectrepository() {
+        return currentProjectRepository;
+    }
 
+    /** Gets the project that the repository holds */
     public ProjectModel getCurrentProject() {
         return currentProject;
     }
 
+    /** Sets the project that the repository holds */
     public void setCurrentProject(ProjectModel project) {
         currentProject = project;
     }
 
+    /** Sets the project that the repository holds to a null reference. */
     public void removeCurrentProject() {
-        currentProject = null;
+        this.setCurrentProject(null);
     }
 
-    public void deleteProject(UUID projectID) {}
-
-    public Project getProjectByID(UUID projectID) {
-        return null;
-    }
 }
