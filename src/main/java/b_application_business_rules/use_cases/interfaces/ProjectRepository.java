22809e37523e2f9cc1390e002d228be64858c56a
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-05 20:58:15 -0400

    commenting out reference files

--- src/main/java/b_application_business_rules/use_cases/interfaces/ProjectRepository.java
+++ src/main/java/b_application_business_rules/use_cases/interfaces/ProjectRepository.java
@@ -1,17 +1,17 @@
-package b_application_business_rules.use_cases.interfaces;
-
-import a_enterprise_business_rules.entities.Project;
-
-import java.util.List;
-import java.util.UUID;
-/**
- * first draft
- */
-public interface ProjectRepository {
-    Project getProjectById(UUID projectId);
-
-    List<Project> getAllProjects();
-
-    void saveProject(Project project);
-}
-
+//package b_application_business_rules.use_cases.interfaces;
+//
+//import a_enterprise_business_rules.entities.Project;
+//
+//import java.util.List;
+//import java.util.UUID;
+///**
+// * first draft
+// */
+//public interface ProjectRepository {
+//    Project getProjectById(UUID projectId);
+//
+//    List<Project> getAllProjects();
+//
+//    void saveProject(Project project);
+//}
+//
--- src/main/java/c_interface_adapters/CSVColumnRepository.java
+++ src/main/java/c_interface_adapters/CSVColumnRepository.java
@@ -1,42 +1,42 @@
-package c_interface_adapters;
-
-import a_enterprise_business_rules.entities.Column;
-import b_application_business_rules.use_cases.interfaces.ColumnRepository;
-import d_frameworks_and_drivers.database_management.DBAdapters.CSVReader;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-/**
- * first draft
- */
-public class CSVColumnRepository implements ColumnRepository {
-    private CSVReader csvReader;
-
-    public CSVColumnRepository(CSVReader csvReader) {
-        this.csvReader = csvReader;
-    }
-
-    @Override
-    public Column getColumnById(UUID columnId) {
-        // Retrieve the CSV record using the columnId and convert it to a Column object
-        // Implement the logic using the csvReader.getUUIDToRecordMap() method
-        return null;
-    }
-
-    @Override
-    public List<Column> getAllColumns() {
-        // Retrieve all CSV records and convert them to a list of Column objects
-        // Implement the logic using the csvReader.getUUIDToRecordMap() method
-        return new ArrayList<>();
-    }
-
-    @Override
-    public void saveColumn(Column column) {
-        // Convert the Column object to a CSV record and save it to the CSV file
-        // Implement the logic using the csvReader.getStringToStringMap() method
-    }
-
-    // Additional methods for managing columns in CSV file
-}
+//package c_interface_adapters;
+//
+//import a_enterprise_business_rules.entities.Column;
+//import b_application_business_rules.use_cases.interfaces.ColumnRepository;
+//import d_frameworks_and_drivers.database_management.DBAdapters.CSVMapper;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.UUID;
+//
+///**
+// * first draft
+// */
+//public class CSVColumnRepository implements ColumnRepository {
+//    private CSVMapper csvMapper;
+//
+//    public CSVColumnRepository(CSVMapper csvMapper) {
+//        this.csvMapper = csvMapper;
+//    }
+//
+//    @Override
+//    public Column getColumnById(UUID columnId) {
+//        // Retrieve the CSV record using the columnId and convert it to a Column object
+//        // Implement the logic using the csvMapper.getUUIDToRecordMap() method
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Column> getAllColumns() {
+//        // Retrieve all CSV records and convert them to a list of Column objects
+//        // Implement the logic using the csvMapper.getUUIDToRecordMap() method
+//        return new ArrayList<>();
+//    }
+//
+//    @Override
+//    public void saveColumn(Column column) {
+//        // Convert the Column object to a CSV record and save it to the CSV file
+//        // Implement the logic using the csvMapper.getStringToStringMap() method
+//    }
+//
+//    // Additional methods for managing columns in CSV file
+//}
