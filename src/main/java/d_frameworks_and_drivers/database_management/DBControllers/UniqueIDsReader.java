a267e03648d78def301c0057d0747052ecd78888
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-10 11:47:33 -0400

    unique id reader update - class not used

--- src/main/java/d_frameworks_and_drivers/database_management/DBControllers/UniqueIDsReader.java
+++ src/main/java/d_frameworks_and_drivers/database_management/DBControllers/UniqueIDsReader.java
@@ -1,12 +1,12 @@
 package d_frameworks_and_drivers.database_management.DBControllers;
 
-import d_frameworks_and_drivers.database_management.DBAdapters.CSVMapper;
+import d_frameworks_and_drivers.database_management.DBAdapters.DBMapper;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
-public class UniqueIDsReader implements c_interface_adapters.UniqueIDsReader {
+public class UniqueIDsReader {
     /**
      * Returns a Mapping from the string of key column field to the string of the value column field.
      * aka convertCsvToHashmap
@@ -15,9 +15,9 @@ public class UniqueIDsReader implements c_interface_adapters.UniqueIDsReader {
     public Map<String, String> getMap() {
         // Use the first header as the key and the second header as the value in the HashMap
         // Assuming the first header is in the 0th column, the second header is in the 1st column
-        try (CSVMapper csvMapper = new CSVMapper("DatabaseFiles/UniqueIDs/UniqueIDs.csv")){
+        try (DBMapper DBMapper = new DBMapper("src/main/java/d_frameworks_and_drivers/database_management/DatabaseFiles/UniqueIDs/UniqueIDs.csv")){
 
-            return csvMapper.getStringToStringMap(0, 1);
+            return DBMapper.getStringToStringMap(0, 1);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
