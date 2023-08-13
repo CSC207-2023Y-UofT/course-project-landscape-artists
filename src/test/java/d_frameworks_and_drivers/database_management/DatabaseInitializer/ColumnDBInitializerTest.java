ebda81d46c21776410f65a6d19cf5da88fa3a47f
Igor Kan <igor.kan.zakhidoff@gmail.com>
2023-08-13 11:19:32 -0400

    column initializer test

--- src/test/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/ColumnDBInitializerTest.java
+++ src/test/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/ColumnDBInitializerTest.java
@@ -1,17 +1,28 @@
 package d_frameworks_and_drivers.database_management.DatabaseInitializer;
 
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ * Tests if ColumnDBInitializer initializes Columns.csv file with correct format.
+ */
 class ColumnDBInitializerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private static final String CSV_FILE_PATH = "src/main/java/d_frameworks_and_drivers/database_management/DatabaseFiles/Columns/Columns.csv";
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void testColumnDBInitializer() {
+        // Check if the CSV file was created and has the expected format
+        try (CSVReader reader = new CSVReader(new FileReader(CSV_FILE_PATH))){
+            String[] header = reader.readNext();
+            assertArrayEquals(new String[]{"ColumnID", "Name", "Task ID's"}, header);
+        } catch (IOException e) {fail("Exception occurred: " + e.getMessage());}
     }
 }
