619bcc965c89a996ee2869be950c426f0b20371b
rtutz <ronaldjohn.tutor@gmail.com>
2023-08-16 09:31:44 -0400

    documented initializers.

--- src/main/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/ColumnDBInitializer.java
+++ src/main/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/ColumnDBInitializer.java
@@ -5,9 +5,17 @@ import com.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileWriter;
 
+/**
+ * The ColumnDBInitializer class is responsible for initializing the CSV file for storing column data.
+ * It creates the file and writes the column headers as the initial content.
+ */
 public class ColumnDBInitializer {
     String[] ColumnHeaders = {"ColumnID", "Name", "Task ID's"};
 
+    /**
+     * Constructs a ColumnDBInitializer object and initializes the CSV file for columns.
+     * It creates the file and writes the column headers as the initial content.
+     */
     public ColumnDBInitializer() {
         // create CSVWriter object filewriter object as parameter
         File file = new File("src/main/java/d_frameworks_and_drivers/database_management/DatabaseFiles/Columns/Columns.csv");
--- src/main/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/UniqueIDsInitializer.java
+++ src/main/java/d_frameworks_and_drivers/database_management/DatabaseInitializer/UniqueIDsInitializer.java
@@ -5,9 +5,17 @@ import com.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileWriter;
 
+/**
+ * The UniqueIDsInitializer class is responsible for initializing the CSV file for storing unique IDs.
+ * It creates the file and writes the headers for UUIDs and their states as the initial content.
+ */
 public class UniqueIDsInitializer {
     String [] IdsDbHeaders = {"UUID", "State"};
 
+    /**
+     * Constructs a UniqueIDsInitializer object and initializes the CSV file for unique IDs.
+     * It creates the file and writes the headers for UUIDs and their states as the initial content.
+     */
     public UniqueIDsInitializer() {
         // create CSVWriter object filewriter object as parameter
         File file = new File("src/main/java/d_frameworks_and_drivers/database_management/DatabaseFiles/UniqueIDs/UniqueIDs.csv");
